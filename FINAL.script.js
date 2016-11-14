bb1908058a316dc5cb6ca7def4010bd6a308beb9
Louis Ritchie <louiscritchie@gmail.com>
2016-11-13 22:15:12 -0800

    :fire: the client code

---
+++ FINAL.script.js
@@ -0,0 +1,113 @@
+class Player {
+  constructor(address, container) {
+    this.address = address;
+
+    // The container node is the node in the DOM into which we will render elements.
+    // Begin by appending the child nodes we will need later.
+    this.nodes = {};
+    for (let name of ['current', 'graph', 'next', 'playlist']) {
+      this.nodes[name] = document.createElement(name == 'playlist' ? 'table' : 'div');
+      this.nodes[name].className = name;
+      container.appendChild(this.nodes[name]);
+    }
+  }
+
+  connect() {
+    // Connect to the socket
+    /* WORKSHOP STEP 1a: Initialize socket connection */
+    this.socket = io(this.address);
+
+    // The playlist is updated when someone votes on a song, when a
+    // new song starts (to zero the votes) and when we first connect.
+    this.socket.on('playlist-update', (songs) => {
+      /* WORKSHOP STEP 1a: Call renderPlaylist method */
+      this.renderPlaylist(songs);
+    });
+
+    // When the socket server emits a 'audio-update' event,
+    // that means there is new spectrum data to render.
+    // This happens very often (once every 50ms plus latency)
+    /* WORKSHOP STEP 2a: Attach renderAudioVisualizer method to "audio-update" event */
+    this.socket.on('audio-update', (data) => {
+      this.renderAudioVisualizer(data);
+    });
+
+    // A play-start event is emitted when a new song starts playing
+    // and when we first connect. Update the 'current' song title.
+    /* WORKSHOP STEP 4a: Attach renderNowPlaying method to "play-start" event */
+    this.socket.on('play-start', (song) => {
+      this.renderNowPlaying(song);
+    });
+  }
+
+  renderAudioVisualizer(data) {
+    // Render a bar for each data point
+    let i, bar, barHeight, graph = this.nodes.graph;
+    for (i = 0; i < data.length; i++) {
+      if (graph.childNodes[i]) {
+        bar = graph.childNodes[i];
+      } else {
+        bar = document.createElement('div');
+        graph.appendChild(bar);
+      }
+
+      /* WORKSHOP STEP 2b: Calculate barHeight based on data[i] variable */
+      barHeight = data[i]/500 * 100;
+      bar.style.height = barHeight + '%';
+    }
+
+    // Remove excess bars
+    while (graph.childNodes.length > i) {
+      graph.removeChild(graph.lastChild);
+    }
+  }
+
+  renderPlaylist(songs){
+    // Clear out existing songs
+    this.nodes.playlist.innerHTML = '';
+
+    // Render each song into playlist table
+    let next = null;
+    for (let song of songs) {
+        let row = document.createElement('tr');
+        for (let property of ['title', 'artist', 'votes']){
+            var cell = document.createElement('td');
+            cell.innerText = song[property];
+            row.appendChild(cell);
+        }
+
+        // Add the vote button
+        cell = row.querySelector('td:last-child');
+        cell.className = 'votes';
+        let voteButton = document.createElement('button');
+        voteButton.innerHTML = '&#x2b06;'; // Unicode "Upwards Black Arrow" character
+
+        // Add the vote event to the button
+        voteButton.addEventListener('click', () => {
+          /* WORKSHOP STEP 3: Emit the "vote-cast" event and pass the "song" variable as data */
+          this.socket.emit('vote-cast', song);
+        });
+
+        cell.appendChild(voteButton);
+        this.nodes.playlist.appendChild(row);
+
+        // Next up is the song with the most votes
+        if (!next || song.votes > next.votes) {
+          next = song;
+        }
+    }
+
+    // Update next-up
+    if (next) {
+      /* WORKSHOP STEP 4c: Set nextSongLabel to title and artist of "next" variable */
+      let nextSongLabel = next.artist + ' - ' + next.title;
+      this.nodes.next.innerText = nextSongLabel;
+    }
+  }
+
+  renderNowPlaying(song) {
+    /* WORKSHOP STEP 4b: Set nowPlayingLabel to title and artist of "song" variable */
+    let nowPlayingLabel = song.artist + ' - ' + song.title;
+    this.nodes.current.innerText = nowPlayingLabel;
+  }
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+#Audio Visualization with WebSockets
+
+![](https://media.giphy.com/media/j3gsT2RsH9K0w/giphy.gif)
+## Before the Workshop
+
+* Install a modern web browser
+  * [Chrome](https://www.google.com/chrome/)
+  * [Firefox](https://www.mozilla.org/en-US/firefox/new/)
+* Get a text editor
+  * Try [Sublime](https://www.sublimetext.com/) or [Atom](https://atom.io/)
+* Download the materials
+  * http://workday.startupslam.io/workshop.zip
+
+## Workshop Instructions
+
+The workshop is broken into 4 parts.  You will be filling in various parts of `script.js` along with everyone.
+
+* `script.js` will be where you perform your work
+* `START.script.js` allows you to reset at any time
+* `FINAL.script.js` is the final result if you need help
+
+The four parts will be:
+
+1. Connecting to socket server and viewing the playlist
+1. Visualizing the audio
+1. Upvoting tracks in the playlist
+1. Now playing and up next
